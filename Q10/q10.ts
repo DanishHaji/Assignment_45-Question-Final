8214b4d9a9bf0c8ed47d6d129bd0bc48a9cf46ac
DanishHaji <160336776+DanishHaji@users.noreply.github.com>
2024-03-29 22:35:02 +0500

    Question 10

---
+++ Q10/q10.ts
@@ -0,0 +1,15 @@
+//Name       : Danish
+//Roll No    : 00405957
+//Batch      : 01
+//Days/Time  : Thursday (07:00PM - 10:00PM)
+
+/*
+ ****************** Question No: 10 ****************************
+Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+ */
+
+// Danish ==> 16/02/2024
+// This program will show you my personal message. 
+
+let myName: string = "Danish";
+console.log(`Hello ${myName}, would you like to learn some Python today?`);
