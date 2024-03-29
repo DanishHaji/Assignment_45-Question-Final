883f1b2c8fdcdddfbca40e45b8eaf942b2dac2d7
DanishHaji <160336776+DanishHaji@users.noreply.github.com>
2024-03-29 22:29:27 +0500

    Question 2

---
+++ Q2/q2.ts
@@ -0,0 +1,12 @@
+//Name       : Danish
+//Roll No    : 00405957
+//Batch      : 01
+//Days/Time  : Thursday (07:00PM - 10:00PM)
+
+/*
+Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+*/
+
+let personName: string = "Danish Haji";
+console.log("hello " + personName + ", would you like to learn some Python Today?");
