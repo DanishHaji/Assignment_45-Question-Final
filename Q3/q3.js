bcea225098ad024bdb089704b566939a08731f3a
DanishHaji <160336776+DanishHaji@users.noreply.github.com>
2024-03-29 22:30:21 +0500

    Question 3

---
+++ Q3/q3.js
@@ -0,0 +1,13 @@
+//Name       : Danish
+//Roll No    : 00405957
+//Batch      : 01
+//Days/Time  : Thursday (07:00PM - 10:00PM)
+/*
+****************** Question No: 3 ****************************
+
+Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+*/
+var myName = "Danish";
+console.log(myName.toLowerCase());
+console.log(myName.toUpperCase());
+console.log(myName.charAt(0).toUpperCase() + myName.slice(1).toLowerCase());
